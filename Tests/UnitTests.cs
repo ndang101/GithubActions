08db832bbb6013d2d8108711c049399a4e774c6e
Nathan Dang <ndang5@huskers.unl.edu>
2025-03-27 20:00:23 -0500

    Fixed issue with test cases where they were in the addition

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -3,145 +3,145 @@ namespace GithubActionsLab;
 [TestClass]
 public class Addition
 {
-	[TestMethod]
-	public void Add_Valid_Dang()
-	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("5", "7"));
-	}
-
-	[TestMethod]
-	public void Add_Invalid_Dang()
-	{
-		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
-	}
-
-	[TestMethod]
-	public void Add_Null_Dang()
-	{
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
-	}
-
-    [TestClass]
-    public class Subtraction
+    [TestMethod]
+    public void Add_Valid_Dang()
     {
-        [TestMethod]
-        public void Subtract_Valid_Dang()
-        {
-            Assert.AreEqual(1, Program.Subtract("3", "2"));
-            Assert.AreEqual(0, Program.Subtract("5", "5"));
-            Assert.AreEqual(-2, Program.Subtract("5", "7"));
-        }
-
-        [TestMethod]
-        public void Subtract_Invalid_Dang()
-        {
-            Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
-            Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
-            Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
-        }
-
-        [TestMethod]
-        public void Subtract_Null_Dang()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
-        }
+        Assert.AreEqual(3, Program.Add("1", "2"));
+        Assert.AreEqual(5, Program.Add("3", "2"));
+        Assert.AreEqual(12, Program.Add("5", "7"));
     }
 
-    [TestClass]
-    public class Multiplication
+    [TestMethod]
+    public void Add_Invalid_Dang()
     {
-        [TestMethod]
-        public void Multiply_Valid_Dang()
-        {
-            Assert.AreEqual(6, Program.Multiply("2", "3"));
-            Assert.AreEqual(0, Program.Multiply("5", "0"));
-            Assert.AreEqual(-14, Program.Multiply("7", "-2"));
-        }
-
-        [TestMethod]
-        public void Multiply_Invalid_Dang()
-        {
-            Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
-            Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
-            Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
-        }
-
-        [TestMethod]
-        public void Multiply_Null_Dang()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
-        }
+        Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
     }
 
-    [TestClass]
-    public class Division
+    [TestMethod]
+    public void Add_Null_Dang()
     {
-        [TestMethod]
-        public void Divide_Valid_Dang()
-        {
-            Assert.AreEqual(2, Program.Divide("6", "3"));
-            Assert.AreEqual(1.5, Program.Divide("3", "2"));
-            Assert.AreEqual(-4, Program.Divide("8", "-2"));
-        }
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+    }
+}
 
-        [TestMethod]
-        public void Divide_Invalid_Dang()
-        {
-            Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
-            Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
-            Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
-        }
-
-        [TestMethod]
-        public void Divide_Null_Dang()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
-        }
-
-        [TestMethod]
-        public void Divide_ByZero_Dang()
-        {
-            Assert.ThrowsException<DivideByZeroException>(() => Program.Divide("5", "0"));
-        }
+[TestClass]
+public class Subtraction
+{
+    [TestMethod]
+    public void Subtract_Valid_Dang()
+    {
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+        Assert.AreEqual(0, Program.Subtract("5", "5"));
+        Assert.AreEqual(-2, Program.Subtract("5", "7"));
     }
 
-    [TestClass]
-    public class Power
+    [TestMethod]
+    public void Subtract_Invalid_Dang()
     {
-        [TestMethod]
-        public void Power_Valid_Dang()
-        {
-            Assert.AreEqual(8, Program.Power("2", "3"));
-            Assert.AreEqual(1, Program.Power("5", "0"));
-            Assert.AreEqual(16, Program.Power("4", "2"));
-        }
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+    }
 
-        [TestMethod]
-        public void Power_Invalid_Dang()
-        {
-            Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
-            Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
-            Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
-        }
+    [TestMethod]
+    public void Subtract_Null_Dang()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+    }
+}
 
-        [TestMethod]
-        public void Power_Null_Dang()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
-            Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
-        }
+[TestClass]
+public class Multiplication
+{
+    [TestMethod]
+    public void Multiply_Valid_Dang()
+    {
+        Assert.AreEqual(6, Program.Multiply("2", "3"));
+        Assert.AreEqual(0, Program.Multiply("5", "0"));
+        Assert.AreEqual(-14, Program.Multiply("7", "-2"));
+    }
+
+    [TestMethod]
+    public void Multiply_Invalid_Dang()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+    }
+
+    [TestMethod]
+    public void Multiply_Null_Dang()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+    }
+}
+
+[TestClass]
+public class Division
+{
+    [TestMethod]
+    public void Divide_Valid_Dang()
+    {
+        Assert.AreEqual(2, Program.Divide("6", "3"));
+        Assert.AreEqual(1.5, Program.Divide("3", "2"), 0.0001); // Added tolerance
+        Assert.AreEqual(-4, Program.Divide("8", "-2"));
+    }
+
+    [TestMethod]
+    public void Divide_Invalid_Dang()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+    }
+
+    [TestMethod]
+    public void Divide_Null_Dang()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+    }
+
+    [TestMethod]
+    public void Divide_ByZero_Dang()
+    {
+        Assert.ThrowsException<DivideByZeroException>(() => Program.Divide("5", "0"));
+    }
+}
+
+[TestClass]
+public class Power
+{
+    [TestMethod]
+    public void Power_Valid_Dang()
+    {
+        Assert.AreEqual(8, Program.Power("2", "3"));
+        Assert.AreEqual(1, Program.Power("5", "0"));
+        Assert.AreEqual(16, Program.Power("4", "2"));
+    }
+
+    [TestMethod]
+    public void Power_Invalid_Dang()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+    }
+
+    [TestMethod]
+    public void Power_Null_Dang()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
     }
 }
